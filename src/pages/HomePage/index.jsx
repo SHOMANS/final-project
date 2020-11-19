a1b3d398885ef98f6cdaa059d9ce4c20037e99ef
SHOMANS <mohammedshoman9@gmail.com>
2020-11-19 03:05:56 +0200

    set up hrmePage

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import "./style.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
